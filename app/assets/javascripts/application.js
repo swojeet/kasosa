bd761cd3cd08788a90c8b9944d81a5b91526b5db
swojeet <swojeetkayastha@gmail.com>
2017-07-21 15:17:50 +0545

    Added Bootstrap 4 to the project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require tether
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .

389ff2d76431bfce0b2819fca8331c5a01448d2b
swojeet <swojeetkayastha@gmail.com>
2017-07-24 23:26:46 +0545

    Improve UI

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,8 @@ class SiteController < ApplicationController
   def index
     @posts = Post.all.order("created_at DESC")
   end
+
+  def gallery
+    @posts = Post.all.order("created_at DESC")
+  end
 end

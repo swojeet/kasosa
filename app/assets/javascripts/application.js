9080abac3aeaeb66f85695f21af0f914eb9362a1
swojeet <swojeetkayastha@gmail.com>
2017-07-30 15:36:22 +0545

    Add jquery-rails to gemfile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery/dist/jquery.min.js
 //= require tether
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
+//= jquery
 //= require_tree .

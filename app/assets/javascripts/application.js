57fc5cb4cc18fe405fbb498945c732b9984254b4
swojeet <swojeetkayastha@gmail.com>
2017-07-30 15:23:06 +0545

    Add jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require tether
 //= require bootstrap-sprockets
 //= require rails-ujs

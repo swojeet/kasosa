7d657a9f76bb5673f7f54033376bdf4b85e7f6d9
swojeet <swojeetkayastha@gmail.com>
2017-07-30 15:24:54 +0545

    Precompile assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery/dist/jquery.min.js
 //= require tether
 //= require bootstrap-sprockets
 //= require rails-ujs
